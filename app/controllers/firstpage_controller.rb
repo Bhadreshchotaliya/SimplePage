1da2e3a417252f307bc69f78cc25554ee61d6559
Mr.bhadresh <bhadreshchotaliya23@gmail.com>
2019-09-25 12:17:27 +0530

    Setting up the routes with home pages

---
+++ app/controllers/firstpage_controller.rb
@@ -0,0 +1,9 @@
+class FirstpageController < ApplicationController
+	def home
+		
+	end
+	
+	def about
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	#get 'firstpage/home', to: 'firstpage#home'
+	
+	root 'firstpage#home'
+	get 'about', to: 'firstpage#about'
 end
